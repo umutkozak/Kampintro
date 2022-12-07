147fbe0202b6e7c9e06875925a59c815c1c4cf0a
umut <umut@DESKTOP-OC87PC6>
2022-12-07 14:23:13 +0300

    3.11

---
+++ Dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Dictionary;
+using Microsoft.VisualBasic;
+Dictionary<string, object> properties = new Dictionary<string, object>();
+properties.Add("umut", 1);
+foreach (var item in properties)
+{
+    Console.WriteLine(item);
+}
+
+//Console.WriteLine(properties);
+
+
+myDictionary<string, int> ogrenciler = new myDictionary<string, int>();
+ogrenciler.Add("umut", 1);
+
+
+//foreach (var item in ogrenciler)
+//{
+  //  Console.WriteLine(item);
+//}
+Console.WriteLine(ogrenciler.Length);
+ogrenciler.Get();
+
---
+++ Dictionary/myDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class myDictionary<Tkey, Tvalue>
+    {
+        Tkey[] k = new Tkey[0];
+        Tvalue[] v=new Tvalue[0];
+
+        public myDictionary()
+        {
+            
+        }
+
+        
+        public void Add(Tkey key,Tvalue value) 
+        {
+            Tkey[] kTemparray = k;
+            k = new Tkey[k.Length + 1];
+            Tvalue[] vTemparray = v;
+            v = new Tvalue[v.Length + 1];
+
+            for (int i = 0; i < kTemparray.Length; i++)
+            {
+                k[i] = kTemparray[i];
+                v[i] = vTemparray[i];
+            }
+            v[v.Length - 1] = value;
+            k[k.Length - 1] = key;
+        }
+
+        public int Length
+        {
+            get { return v.Length; }
+
+        }
+        public int Count()
+        {
+
+            return k.Length;
+        }
+        public void Get()
+        {
+            for (int i = 0; i < Count(); i++)
+            {
+                Console.WriteLine( "k[i]"  , v[i]);
+            }
+        }
+    }
+
+}
+
